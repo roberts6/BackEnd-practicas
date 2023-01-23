e9c938c859056dd2c4b37173afcdd12b5a26495b
roberts6 <roberts_oscar@hotmail.com>
2023-01-23 13:45:46 -0300

    servidor andando, problemas para importar index al app.js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,26 @@
 const express = require("express")
 const app = express()
 
+//import
+import index from './index.js'
+
+// middleware
+app.use(express.urlencoded({extended: true}));
+
 // rutas
 app.get('/', (req, res) => {
-res.send('Raiz del proyecto')
+res.send('Raiz del proyecto!!!')
 })
 
 app.get('/products', (req, res) => {
-res.send('Raiz del proyecto')
+res.send('Total de productos')
+})
+
+app.get('/products/:pid', (req, res) => {
+let productId = req.params.productId;
+let product = products.find(product => product.id == productId)
+// if (product) return res.send(product)
+product ? res.send(product) : res.send("producto no encontrado")
 })
 
 // chequeo de servidor y manejo de error
